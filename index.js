a746f460e163a8f8f42d1b21bb553baea283fb53
Mohussainsayyed <127851686+Mohussainsayyed@users.noreply.github.com>
2023-10-09 14:52:18 +0530

    Add files via upload
    Added animation by gsap

--- index.js
+++ index.js
@@ -56,3 +56,132 @@
 	  				message => alert("sorry this is not working")
 	  				);
 	  			}
+	  			/*animation gsap start*/
+	  			
+	  			let tl = gsap.timeline();
+	  			
+	  			tl.from(".home h1,.home h3,.home p",{
+	  			stagger:.4,
+	  			x:-100,
+	  			opacity:0,
+	  			});
+	  			
+	  			tl.from(".home .btn-box",{
+	  			y:100,
+	  			opacity:0,
+	  			duration:.5,
+	  			});
+	  			
+	  			tl.from(".home .home-sci a" ,{
+	  			y:100,
+	  			opacity:0,
+	  			duration:.5,
+	  			stagger:.1,
+	  			});
+	  			/*about*/
+	  			
+	  			gsap.from(".about .heading",{
+	  			y:"-100",
+	  			rotate:"-25deg",
+	  			opacity:0,
+	  			duration:.5,
+	  			scrollTrigger:{
+	  			trigger:(".about .heading"),
+	  			scroller:"body",
+	  			start:"top center",
+	  			end:"center center",
+	  			scrub:2,
+	  			}
+	  			});
+	  			
+	  			gsap.from(".about .about-img",{
+	  			scale:0.8,
+	  			opacity:0,
+	  			duration:.5,
+	  			scrollTrigger:{
+	  			trigger:(".about .about-img"),
+	  			scroller:"body",
+	  			start:"top center",
+	  			end:"center center",
+	  			scrub:2,
+	  			}
+	  			});
+	  			/*education*/
+	  			let tl2 = gsap.timeline({
+	  				scrollTrigger:{
+	  				trigger:(".education"),
+	  				scroller:"body",
+	  				start:"top center",
+	  				end:"center center",
+	  				}
+	  			});
+	  			
+	  			tl2.from(".education .heading",{
+	  			scale:0.8,
+	  			opacity:0,
+	  			duration:.5,
+	 
+	  			});
+	  			
+	  			tl2.from(".education .education-row",{
+	  			scale:0.8,
+	  			opacity:0,
+	  			duration:.5,
+	  			
+	  			});
+	  			
+	  			/*skills*/
+	  			
+	  let tl3 = gsap.timeline({
+	  	scrollTrigger:{
+	  	trigger:(".skills"),
+	  	scroller:"body",
+	  	start:"top center",
+	  	end:"top 30%",
+	  	scrub:1,
+	  	}
+	  });
+	  
+	  tl3.from(" .skills .heading",{
+	  scale:0.8,
+	  translate:"-50px 50px",
+	  opacity:0,
+	  duration:.5,
+	  	 
+	  });
+	  
+	  tl3.from(".skills .skills-row",{
+	  scale:0.8,
+	  opacity:0,
+	  duration:.5,
+	  
+	  });
+	  
+	  
+	  /*contact*/
+	  
+	  let tl4 = gsap.timeline({
+	  scrollTrigger:{
+	  trigger:(".contact"),
+	  scroller:"body",
+	  start:"top center",
+	  end:"center center",
+	  scrub:1,
+	  }
+	  });
+	  
+	  tl4.from(" .contact .heading",{
+	  scale:0.8,
+	  translate:"50px 50px",
+	  opacity:0,
+	  duration:.5,
+	  
+	  });
+	  
+	  tl4.from(".contact form ",{
+	  scale:0.8,
+	  opacity:0,
+	  duration:.5,
+	  
+	  });
+	  
